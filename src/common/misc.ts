0c1fc7c4412ba2df789eb42d58f38a3aa26bfb91
stan3984 <staffan.aw@protonmail.com>
2020-11-12 10:08:33 +0100

    fix interpolate()

--- src/common/misc.ts
+++ src/common/misc.ts
@@ -20,7 +20,7 @@ export interface Input {
  * @param delta Interpolation percentage (should be in the interval [0, 1])
  */
 export function interpolate(src: Vec2, dst: Vec2, delta: number): Vec2 {
-  const x = Vec2.mul(src, delta);
-  const y = Vec2.mul(dst, 1 - delta);
+  const x = Vec2.mul(src, 1 - delta);
+  const y = Vec2.mul(dst, delta);
   return Vec2.add(x, y);
 }
