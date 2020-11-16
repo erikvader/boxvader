630b2b108d264b3186863d2d4f7c2f83b0bea4c0
stan3984 <staffan.aw@protonmail.com>
2020-11-16 14:06:26 +0100

    move tilesheet to public/imgs/tilesheets

--- levels/vov-scifi-tileset.tsx
+++ levels/vov-scifi-tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="vov-scifi" tilewidth="32" tileheight="32" tilecount="84" columns="14">
- <image source="scifitiles-sheet.png" width="448" height="192"/>
+ <image source="../public/imgs/tilesheets/scifitiles-sheet.png" width="448" height="192"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="32" height="12"/>
