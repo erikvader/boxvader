6fab36a8386373cf8d7929d3940500905cd1edf9
stan3984 <staffan.aw@protonmail.com>
2020-11-17 10:14:37 +0100

    remove x? from webpack module rules pattern

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts$/,
         use: 'ts-loader?configFile=tsconfig.webpack.json',
       },
     ],
