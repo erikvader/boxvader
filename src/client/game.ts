5486f461b559543855ca7ac2916a4af432d864ec
ponterik <pontus.z.eriksson@outlook.com>
2020-12-17 15:39:59 +0100

    stylecheck

--- src/client/game.ts
+++ src/client/game.ts
@@ -247,17 +247,16 @@ export default class ClientGame extends GameLoop {
 
     for (const player_id in this.player_list) {
       if (!newState.players[player_id].alive) {
-        this.player_list[player_id].hpBar.visible=false
+        this.player_list[player_id].hpBar.visible = false;
         this.player_list[player_id].visible = false;
         if (this.player_list[player_id].shot_line !== undefined) {
           this.player_list[player_id].shot_line.visible = false;
         }
       } else {
-        if( this.player_list[player_id].visible===false){
+        if (this.player_list[player_id].visible === false) {
           this.player_list[player_id].visible = true;
-          this.player_list[player_id].hpBar.visible=true;
+          this.player_list[player_id].hpBar.visible = true;
         }
-        
       }
     }
   }
